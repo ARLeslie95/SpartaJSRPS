e74e7f176c8fd6ff88be55f69ddb64348f5c1839
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-04-09 20:56:27 +0100

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# SpartaJSRPS
+# Sparta Rock, Paper, Scissors - javascript
+
+## Task
+
+Create an interactive rock, paper scissors game using JavaScript. Features must include:
+
+- Get the player input through the browser
+- Play against the computer and create a random move for it
+- Check and announce score after every round
+- Play the best out of five games
+- Announce the winner at the end
+
+## Result
+
+Used functions with user prompts and a random number generator to receive generate the computer's move, and combine it with the user's move to determine outcome of the round. Then put the functions in a for loop to run until the maximum score was reached.
+
+## Instructions
+
+Clone this git repository. Open index.html locally in your browser.
--- main.js
+++ main.js
@@ -31,11 +31,12 @@
 // }
 
 function game(){
-
+  alert("Welcome to Rock, Paper, Scissors!");
+  alert("The first to score 3 points wins the game. Good luck!");
   var pScore = 0;
   var cScore = 0;
   while (pScore < 3 && cScore < 3){
-    var move = prompt("Please enter your move");
+    var move = prompt("Please enter your move: Rock(r), Paper(p) or Scissors(s)");
 
     function compMove() {
     var random = Math.floor(Math.random() * 3);
@@ -73,15 +74,26 @@ function game(){
 
     if (outcome == "w"){
       pScore += 1;
+      alert("You win! The score is now : Player- " + pScore + " and CPU- " + cScore);
     }
     else if (outcome == "l"){
       cScore += 1;
+      alert("You lose! The score is now : Player- " + pScore + " and CPU- " + cScore);
+
     }
     else if (outcome == "t"){
+      alert("It's a tie! The score is : Player- " + pScore + " and CPU- " + cScore);
 
     }
     console.log(pScore);
     console.log(cScore);
+  }
+
+  if (pScore == 3){
+    alert("Congratulations! You've won a game of chance!")
+  }
+  else if (cScore == 3){
+    alert("Oh no! You've lost. Maybe try playing Heads or Tails")
   }
 }
 game();
